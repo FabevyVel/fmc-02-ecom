30b1783e544609170c6521d610c71e84289ac7fc
FabevyVel <vel@fabevy.com>
2022-01-23 18:09:33 +0530

    hero component templating

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -8,6 +8,63 @@ import { Component, OnInit } from '@angular/core';
 export class HeroComponent implements OnInit {
 
   constructor() { }
+  departments = [
+    {
+      name: 'Fresh Meat',
+      link: '#'
+    },
+    {
+      name: 'Vegetables',
+      link: '#'
+    },
+    {
+      name: 'Fruit & Nut Gifts',
+      link: '#'
+    },
+    {
+      name: 'Fresh Berries',
+      link: '#'
+    },
+    {
+      name: 'Ocean Foods',
+      link: '#'
+    },
+    {
+      name: 'Butter & Eggs',
+      link: '#'
+    },
+    {
+      name: 'Fastfood',
+      link: '#'
+    },
+    {
+      name: 'Fresh Onion',
+      link: '#'
+    },
+    {
+      name: 'Papayaya & Crisps',
+      link: '#'
+    },
+    {
+      name: 'Oatmeal',
+      link: '#'
+    },
+    {
+      name: 'Fresh Bananas',
+      link: '#'
+    }
+  ];
+
+  contact = '+65 11.188.888';
+
+  hero = {
+    image: 'assets/img/hero/banner.jpg',
+    tag: 'Fruit Fresh',
+    title: '100% Organic',
+    description: 'Free Pickup and Delivery Available',
+    cta: 'Shop Now',
+    link: '#'
+  }
 
   ngOnInit(): void {
   }
