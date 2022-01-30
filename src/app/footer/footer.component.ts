e6d4ae41144e3b3bf552917f262880ccf895b098
janakiraman <jrdurairajani@gmail.com>
2022-01-30 20:18:37 +0530

    Blog and Footer updated

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  footContact = [
+    {
+      img: "assets/img/xlogo.png.pagespeed.ic.ap5icp--OT.png",
+      link: "index-2.html",
+      address: "Address: 60-49 Road 11378 New York",
+      phone: "Phone: +65 11.188.888",
+      email: "Email",
+      emailId: "https://preview.colorlib.com/cdn-cgi/l/email-protection",
+      emailTxt: "[email&#160;protected]",
+    }
+  ];
+  footUsefultitle: "Useful Links";
+  footUsefulInfos = [
+    {     
+      widget1: "About Us",
+      widget1Link: "#",
+      widget2: "About Our Shop",
+      widget2Link: "#",
+      widget3: "Secure Shopping",
+      widget3Link: "#",
+      widget4: "Delivery infomation",
+      widget4Link: "#",
+      widget5: "Privacy Policy",
+      widget5Link: "#",
+      widget6: "Our Sitemap",
+      widget6Link: "#",
+    },
+    {      
+      widget1: "Who We Are",
+      widget1Link: "#",
+      widget2: "Our Services",
+      widget2Link: "#",
+      widget3: "Projects",
+      widget3Link: "#",
+      widget4: "Contact",
+      widget4Link: "#",
+      widget5: "Innovation",
+      widget5Link: "#",
+      widget6: "Testimonials",
+      widget6Link: "#",
+    }
+  ]
 
+footWidget = [{
+  widgetTitle: "Join Our Newsletter Now",
+  widgetTxt: "Get E-mail updates about our latest shop and special offers.",
+  widgetBtn: "Subscribe",  
+}]
+footLogo = "assets/img/xlogo.png.pagespeed.ic.ap5icp--OT.png"
   constructor() { }
 
   ngOnInit(): void {
--- src/app/from-blog/from-blog.component.ts
+++ src/app/from-blog/from-blog.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FromBlogComponent implements OnInit {
 
+  blogTitles = [
+    {
+      title: "From The Blogs"
+    }
+  ]
+
+  brdr= ["blog__item__pic", "brdr"];
+
+  isControl = true;
+  paddControl = {
+    blog__item__text: this.isControl
+  }
+  
+
+  blogItems = [
+    {
+      blogImg: "assets/img/blog/xblog-1.jpg.pagespeed.ic.LxmSdcElIm.jpg",
+      calendar: "May 4,2019",
+      comment: 5,
+      blogTitle: "Cooking tips make cooking simple",
+      blogTxt: "Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat"
+    },
+    {
+      blogImg: "assets/img/blog/xblog-2.jpg.pagespeed.ic.BEPbl29gdJ.jpg",
+      calendar: "May 4,2019",
+      comment: 5,
+      blogTitle: "6 ways to prepare breakfast for 30",
+      blogTxt: "Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat"
+    },
+    {
+      blogImg: "assets/img/blog/xblog-3.jpg.pagespeed.ic.cmlGgG7W6t.jpg",
+      calendar: "May 4,2019",
+      comment: 5,
+      blogTitle: "Visit the clean farm in the US",
+      blogTxt: "Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat"
+    }
+  ];
+
+
   constructor() { }
 
   ngOnInit(): void {
